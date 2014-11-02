fe89d1489daaf6aa399ffd1d9672f49ccea69984
Jackson Burlew <jackburlew5@gmail.com>
2014-11-02 15:46:39 -0500

    Completed first-fit, best-fit, worst-fit. makefile command "make test" will run all input files.

--- hole-mem.c
+++ hole-mem.c
@@ -1,5 +1,10 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <limits.h>
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <math.h>
 
 struct hole {
 	void* location;
@@ -11,6 +16,71 @@ struct hole {
 static struct hole* head;
 static void* mem_start;
 static uint64_t mem_finish;
+
+// A function which is currently disabled that shows color-coded output. It
+// needs a fairly large stack-size to work, so if you wish to use it, you have
+// to remove the #define statement following it, and increase the per-thread
+// stack size.
+#define DEBUG_LEVEL 0b11111111
+static void debug(int level, const char *fmt, ...) {
+	if (!(level & DEBUG_LEVEL))
+		return;
+	va_list args;
+	va_start(args, fmt);
+
+	// Provide color coding.
+	fprintf(stderr, "\033[%dm", 31 + (int) log2(level));
+	vfprintf(stderr, fmt, args);
+	fprintf(stderr, "\033[00m");
+	va_end(args);
+}
+#define debug(...)
+
+void add_hole(void* location, uint32_t size) {
+	struct hole *trace = head, *trail = NULL;
+	while (trace != NULL) {
+		if (location <= trace->location && (trail == NULL || location >= trail->location)) {
+			if ((trail != NULL) && (trail->location + trail->size == location) && (location + size == trace->location)) {
+				// merge the two holes.
+				trail->next = trace->next;
+				trail->size += size + trace->size;
+				free(trace);
+			} else if ((trail != NULL) && (trail->location + trail->size == location)) {
+				// merge the trailing hole with this hole.
+				trail->size += size;
+			} else if (location + size == trace->location) {
+				// merge the next hole with this hole.
+				trace->location -= size;
+				trace->size += size;
+			} else {
+				// create new hole.
+				struct hole* node = malloc(sizeof(struct hole));
+				node->location = location;
+				node->size = size;
+				if (trail == NULL)
+					head = node;
+				else
+					trail->next = node;
+				node->next = trace;
+			}
+			return;
+		}
+		trail = trace;
+		trace = trace->next;
+	}
+	// the hole should be added to the end of the list.
+	if (trail->location + trail->size == location) {
+		// merge with the final node.
+		trail->size += size;
+	} else {
+		// create new node.
+		struct hole* node = malloc(sizeof(struct hole));
+		node->location = location;
+		node->size = size;
+		trail->next = node;
+		node->next = NULL;
+	}
+}
 
 void hole_mem_setup(int malloc_type, uint64_t mem_size, void* start_of_memory) {
 	head = malloc(sizeof(struct hole));
@@ -55,17 +125,120 @@ void* firstfit(int size) {
 			*((uint32_t*) addr) = length;
 			return addr + 4;
 		}
+		trail = trace;
+		trace = trace -> next;
 	}
 	return (void*) -1;
 }
 void* bestfit(int size) {
+	size += 4; // we need 4 bytes to hold the header information.
+	struct hole *trace = head, *trail = NULL;
+	struct hole *best = NULL, *besttrail = NULL;
+	int bestsize = INT_MAX;
+	while (trace != NULL) {
+		if (trace->size >= size && bestsize > trace->size) {
+			best = trace;
+			besttrail = trail;
+			bestsize = trace->size;
+		}
+		trail = trace;
+		trace = trace -> next;
+	}
+
+	// Allocate wherever the new memory space fits best.
+	if (best != NULL) {
+		if (best->size == size) {
+			// Get the address of the hole 
+			void* addr = best->location;
+			uint32_t length = size;
+
+			// the entire hole is filled.
+			if (besttrail != NULL) {
+				besttrail->next = best->next;
+			} else {
+				head = best->next;
+			}
+
+			// free the hole.
+			free(best);
+
+			// write the size of the allocation in the memory region.
+			*((uint32_t*)addr) = length;
+			return addr + 4;
+		} else if (best->size > size) {
+			// Get the address of the hole.
+			void* addr = best->location;
+			uint32_t length = size;
+
+			// Shrink the hole.
+			best->size = best->size - size;
+			best->location = best->location + size;
+
+			// Write the size of the allocation in the memory region.
+			*((uint32_t*) addr) = length;
+			return addr + 4;
+		}
+	}
+
 	return (void*) -1;
 }
 void* worstfit(int size) {
+	size += 4; // we need 4 bytes to hold the header information.
+	struct hole *trace = head, *trail = NULL;
+	struct hole *best = NULL, *besttrail = NULL;
+	int bestsize = 0;
+	while (trace != NULL) {
+		if (trace->size >= size && trace->size > bestsize) {
+			best = trace;
+			besttrail = trail;
+			bestsize = trace->size;
+		}
+		trail = trace;
+		trace = trace -> next;
+	}
+
+	// Allocate wherever the new memory space fits best.
+	if (best != NULL) {
+		if (best->size == size) {
+			// Get the address of the hole 
+			void* addr = best->location;
+			uint32_t length = size;
+
+			// the entire hole is filled.
+			if (besttrail != NULL) {
+				besttrail->next = best->next;
+			} else {
+				head = best->next;
+			}
+
+			// free the hole.
+			free(best);
+
+			// write the size of the allocation in the memory region.
+			*((uint32_t*)addr) = length;
+			return addr + 4;
+		} else if (best->size > size) {
+			// Get the address of the hole.
+			void* addr = best->location;
+			uint32_t length = size;
+
+			// Shrink the hole.
+			best->size = best->size - size;
+			best->location = best->location + size;
+
+			// Write the size of the allocation in the memory region.
+			*((uint32_t*) addr) = length;
+			return addr + 4;
+		}
+	}
+
 	return (void*) -1;
 }
 
 // it is possible that head is NULL.
 void regfree(void* ptr) {
-
+	uint32_t size;
+	ptr -= 4;
+	size = *((uint32_t*) ptr);
+	add_hole(ptr, size);
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 CFLAGS=-Wno-pointer-to-int-cast -Wformat=0 -std=c99 -g
-LINKFLAGS=-Wno-pointer-to-int-cast
+LINKFLAGS=-Wno-pointer-to-int-cast -lm
 OBJFILES=my_memory.o hole-mem.o
 TESTFILES=input/test1.o input/test2.o input/test3.o input/test4.o input/test5.o input/test6.o input/test7.o input/test8.o
 OUTPUT_FILES=test1_output.txt test2_output.txt test3_output.txt test4_output.txt test5_output.txt test6_output.txt test7_output.txt test8_output.txt 
@@ -16,6 +16,16 @@ main: $(OBJFILES) $(TESTFILES)
 	gcc -o test6 $(OBJFILES) input/test6.o $(LINKFLAGS)
 	gcc -o test7 $(OBJFILES) input/test7.o $(LINKFLAGS)
 	gcc -o test8 $(OBJFILES) input/test8.o $(LINKFLAGS)
+
+test: main
+	./test1; diff test1_output.txt output/test1_output.txt || echo '\033[1;31mFailed on test 1 \033[00m'
+	./test2; diff test2_output.txt output/test2_output.txt || echo '\033[1;31mFailed on test 2 \033[00m'
+	./test3; diff test3_output.txt output/test3_output.txt || echo '\033[1;31mFailed on test 3 \033[00m'
+	./test4; diff test4_output.txt output/test4_output.txt || echo '\033[1;31mFailed on test 4 \033[00m'
+	./test5; diff test5_output.txt output/test5_output.txt || echo '\033[1;31mFailed on test 5 \033[00m'
+	./test6; diff test6_output.txt output/test6_output.txt || echo '\033[1;31mFailed on test 6 \033[00m'
+	./test7; diff test7_output.txt output/test7_output.txt || echo '\033[1;31mFailed on test 7 \033[00m'
+	./test8; diff test8_output.txt output/test8_output.txt || echo '\033[1;31mFailed on test 8 \033[00m'
 
 clean:
 	rm -f $(OBJFILES) $(OUTPUT_FILES) $(EXECUTABLES) $(TESTFILES)
